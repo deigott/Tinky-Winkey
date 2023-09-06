cbe99e1213b45dd131c9145147dbdea819c06153
Dei Gott <dollyetal@proton.me>
2023-09-06 09:26:33 -0700

    Adding tinky struct folder.

---
+++ svc/Tinkey.cpp
@@ -0,0 +1,11 @@
+#include "Tinkey.hpp"
+
+Tinkey::Tinkey( void )
+{
+	return ;
+}
+
+Tinkey::Tinkey( const Tinkey& copy )
+{
+	return ;
+}
---
+++ svc/Tinkey.hpp
@@ -0,0 +1,14 @@
+#ifndef TINKEY_HPP
+# define TINKEY_HPP
+# include <iostream>
+# include <string>
+
+class Tinkey {
+public:
+	Tinkey( void );
+	Tinkey( const Tinkey &copy );
+	~Tikney( void );
+private:
+
+};
+#endif
