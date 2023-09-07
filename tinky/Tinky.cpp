ce2fdb50ba7f75a46a2c7a820aa967aec8525b6c
Dei Gott <dollyetal@proton.me>
2023-09-07 17:39:29 +0100

    Implementing start service object.

--- tinky/Tinky.cpp
+++ tinky/Tinky.cpp
@@ -1,11 +1,11 @@
 #include "Tinky.hpp"
 
-Tinky::Tinky( LPCWSTR serviceName )
+Tinky::Tinky( const std::string &serviceName ): _tinkyServiceName(serviceName)
 {
 	/** TO-DO: Establishes a connection to the service control manager
 	 ** And opens the specified service control manager database.
 	 **/
-	_hServiceControlManager =  OpenSCManagerW(
+	_hServiceControlManager =  OpenSCManager(
 		NULL,
 		NULL,
 		SC_MANAGER_ALL_ACCESS
@@ -17,16 +17,14 @@ Tinky::Tinky( LPCWSTR serviceName )
 		exit(EXIT_FAILURE);
 	}
 
-	// Correctly copy the service name
-	_tinkyServiceName = serviceName;
 	return ;
 }
 
-//Tinky::Tinky( const Tinky& copy )
-//{
-//	*this = copy;
-//	return ;
-//}
+Tinky::Tinky( const Tinky& copy )
+{
+	*this = copy;
+	return ;
+}
 
 Tinky::~Tinky( void )
 {
@@ -34,23 +32,30 @@ Tinky::~Tinky( void )
 	return ;
 }
 
-LPCWSTR Tinky::getServiceName(void) const
+std::string Tinky::getServiceName(void) const
 {
 	return (_tinkyServiceName);
 }
 
 bool Tinky::createService(void)
 {
-	LPWSTR	*__ipFileName = new LPWSTR[1024];
-	DWORD	__nSize;
+	BOOL	__closeHandleStatus;
+	char Path[260];
+
+	if (!GetCurrentDirectory(260, Path))
+	{
+		printf("Cannot install service (%ld)\n", GetLastError());
+		return false;
+	}
+	strcat_s(Path, "\\svc.exe");
 
 	/**
 	* Check if a service is already installed/created if not install/create it
 	*/
-	_hServiceTinky = OpenServiceW(
+	_hServiceTinky = OpenService(
 		_hServiceControlManager,
-		_tinkyServiceName,
-		SC_MANAGER_ALL_ACCESS
+		_tinkyServiceName.c_str(),
+		GENERIC_ALL
 	);
 
 	if (_hServiceTinky != NULL)
@@ -66,25 +71,25 @@ bool Tinky::createService(void)
 	* Retrieves the fully qualified path for the file that contains the specified module.
 	* The module must have been loaded by the current process.
 	*/
-	__nSize = GetModuleFileNameW(
-		NULL,
-		*__ipFileName,
-		1024
-	);
-	if (__nSize < 1024)
-		__ipFileName[__nSize] = '\0';
+	//__nSize = GetModuleFileNameW(
+	//	NULL,
+	//	*__ipFileName,
+	//	1024
+	//);
+	//if (__nSize < 1024)
+	//	__ipFileName[__nSize] = '\0';
 	
 
 	/* Creates a service object and adds it to the specified service control manager database. */
-	_hServiceTinky = CreateServiceW(
+	_hServiceTinky = CreateService(
 		_hServiceControlManager,
-		L"tinky",
-		L"tinky",
+		_tinkyServiceName.c_str(),
+		_tinkyServiceName.c_str(),
 		SERVICE_ALL_ACCESS,
 		SERVICE_WIN32_OWN_PROCESS,
 		SERVICE_DEMAND_START,
 		SERVICE_ERROR_NORMAL,
-		*__ipFileName,
+		Path,
 		NULL,
 		NULL,
 		NULL,
@@ -95,32 +100,87 @@ bool Tinky::createService(void)
 		return (false);
 	/** Closes the service object handle **/
 
-	CloseServiceHandle(
+	__closeHandleStatus = CloseServiceHandle(
 		_hServiceTinky
 	);
+	if (__closeHandleStatus == 0)
+	{
+		std::cout << "(-) failed to close service {tinky} handle." << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	return true;
 }
 
 bool Tinky::startService(void)
 {
+	//LPWSTR* __ipFileName = new LPWSTR[1024];
+	BOOL	__closeHandleStatus;
+	BOOL	__startServiceStatus;
+	//DWORD	__nSize;
+
+
 	/**
 	* Check if a service is already installed/created if not install/create it
 	*/
-	_hServiceTinky = OpenServiceW(
+	_hServiceTinky = OpenService(
 		_hServiceControlManager,
-		_tinkyServiceName,
-		SC_MANAGER_ALL_ACCESS
+		_tinkyServiceName.c_str(),
+		GENERIC_ALL
 	);
 
-	if (_hServiceTinky != NULL)
+	if (_hServiceTinky == NULL)
 	{
-		/** Closes the service object handle **/
-		CloseServiceHandle(_hServiceTinky);
-
-		std::cout << "(-) service {tinky} is already installed." << std::endl;
+		std::cout << "(-) service {tinky} is not installed." << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	return false;
+
+	/*__nSize = GetFullPathNameW(
+		L".\\hello.exe",
+		1024,
+		*__ipFileName,
+		NULL
+	);
+	std::cout << __ipFileName << std::endl;*/
+	// Path to the executable you want to run as the service
+	std::string exePath = "C:\\Users\\Devicekiller402\\Projects\\tinky-winkey\\tinky\\hello.exe"; // Update this with the correct path
+
+	
+	char Path[260];
+
+	if (!GetCurrentDirectory(260, Path))
+	{
+		printf("Cannot install service (%ld)\n", GetLastError());
+		return false;
+	}
+	strcat_s(Path, "\\svc.exe");
+
+	// Prepare an array of null-terminated strings
+	LPCSTR serviceArgs[] = { Path, nullptr };
+	/** Starts a service object. **/
+	__startServiceStatus =  StartService(
+		_hServiceTinky,
+		1,
+		serviceArgs
+	);
+
+	if (__startServiceStatus == 0)
+	{
+		std::cout << "(-) failed to start service {tinky} object. ERROR: " << GetLastError() << " "  << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	/** Closes the service object handle **/
+	__closeHandleStatus = CloseServiceHandle(
+		_hServiceTinky
+	);
+	if (__closeHandleStatus == 0)
+	{
+		std::cout << "(-) failed to close service {tinky} handle." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	//delete __ipFileName;
+
+	return true;
 }
 
 bool Tinky::stopService(void)
